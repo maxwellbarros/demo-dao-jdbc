2058dc5d7bde792f0d7f56bfd7f2a15cc8b17122
Maxwell Barros <maxwellbarros@yahoo.com.br>
2021-11-20 15:08:43 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/SellerDaoJDBC.java
@@ -0,0 +1,44 @@
+package model.dao;
+
+import java.sql.Connection;
+import java.util.List;
+
+import model.entities.Seller;
+
+public class SellerDaoJDBC implements SellerDao  {
+
+	public SellerDaoJDBC(Connection connection) {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
