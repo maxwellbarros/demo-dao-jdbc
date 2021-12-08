83909c881dc5410d4374a22be2a55aad3a42438f
Maxwell Barros <maxwellbarros@yahoo.com.br>
2021-12-08 19:45:55 -0300

    DepartmentDao implementation and test

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -7,5 +7,9 @@ public class DaoFactory {
 		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB.getConnection());
+	}
+	
 
 }
